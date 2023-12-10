14194ddd082cce21a3daae0470debbec777cd961
yelsiño <yelsino321@gmai.com>
2023-12-10 00:10:12 -0500

    cambios fetch

---
+++ src/libs/interfaces.ts
@@ -0,0 +1,11 @@
+
+export interface Product {
+  SKU:                         string;
+  "Product title":             string;
+  "Product main image":        string;
+  "Product Details Page Link": string;
+  "Sale price":                string;
+  Commission:                  string;
+  "Promotion Link":            string;
+  likes:                       string;      
+}
